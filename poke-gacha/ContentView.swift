8b9644f883d743698c230e892a037754b5cf8f5f
jamesellisunc <jamellis@ad.unc.edu>
2025-11-16 22:57:17 -0500

    Initial Commit

---
+++ poke-gacha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  poke-gacha
+//
+//  Created by James Ellis on 11/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ poke-gacha/poke_gachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  poke_gachaApp.swift
+//  poke-gacha
+//
+//  Created by James Ellis on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct poke_gachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
